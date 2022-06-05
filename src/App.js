68010e5a54b35fc6d982f9befd8b5e8423c6ba41
Stas Sytnykov <stas82kak05@gmail.com>
2022-06-05 16:09:14 +0300

    add more practice with react router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
+import HomeView from "./views/HomeView/HomeView";
+import EventsView from "./views/EventsView/EventsView";
+import EventSubView from "./views/EventSubView/EventSubView";
+import EventDetailsView from "./views/EventsDetailsView/EventDetailsView";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomeView />} />
+        <Route path={"events"} element={<EventsView />}>
+          <Route path={":eventId"} element={<EventSubView />} />
+        </Route>
+        <Route
+          path={"events/:eventId/details"}
+          element={<EventDetailsView />}
+        />
+        {/* <Route path={"*"} element={<NotFoundView />} /> */}
+      </Route>
+    </Routes>
   );
 }
 
